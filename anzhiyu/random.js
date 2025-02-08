b2f5fbe17f4d318cf7417eab2df49cd5fe3e64e9
AiY0u <2763226573@qq.com>
2025-02-08 18:00:30 +0800

    Site updated: 2025-02-08 18:00:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/25/hello-world/","2025/01/25/试写第一篇文章/","2025/02/02/CRYPTO中的XOR问题/","2025/02/03/CRYPTO中的数论基础（更新中）/"];function toRandomPost(){
+var posts=["2025/01/25/试写第一篇文章/","2025/02/02/CRYPTO中的XOR问题/","2025/02/03/跟随Cryptohack学习数论(上篇)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
