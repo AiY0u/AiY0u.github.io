6de87ccb36ce456b87acea63f3acdbebc7e35b30
AiY0u <2763226573@qq.com>
2025-07-14 14:39:43 +0800

    Site updated: 2025-07-14 14:39:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/02/CRYPTO中的XOR问题/","2025/02/03/跟随Cryptohack学习数论(上篇)/","2025/02/09/山河不足重，重在遇知己/","2025/02/09/使用python质因数分解/","2025/07/09/中国剩余定理解题实例/","2025/07/09/使用勒让德符号求解flag例题/","2025/07/10/Cryptohack-ModularBinomials/","2025/07/11/AES加密详细解释/","2025/07/11/扩散与混淆/","2025/07/11/有限域GF-2⁸-上的运算法则/","2025/07/13/CopperSmith方法寻找小整数根/"];function toRandomPost(){
+var posts=["2025/02/02/CRYPTO中的XOR问题/","2025/02/03/跟随Cryptohack学习数论(上篇)/","2025/02/09/山河不足重，重在遇知己/","2025/02/09/使用python质因数分解/","2025/07/09/中国剩余定理解题实例/","2025/07/09/使用勒让德符号求解flag例题/","2025/07/10/Cryptohack-ModularBinomials/","2025/07/11/AES加密详细解释/","2025/07/11/扩散与混淆/","2025/07/11/有限域GF-2⁸-上的运算法则/","2025/07/13/CopperSmith方法寻找小整数根/","2025/07/14/群/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
